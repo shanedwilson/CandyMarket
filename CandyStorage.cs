090e9cb21a75134eb2868c76df55dc54e99f1b4c
Shane D Wilson <me@shanedwilson.com>
2019-03-18 19:08:09 -0500

    base files

---
+++ CandyStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace candy_market
+{
+    internal class CandyStorage
+    {
+        static List<Candy> _myCandy = new List<Candy>();
+
+        internal IList<string> GetCandyTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Candy SaveNewCandy(Candy newCandy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace candy_market
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var db = SetupNewApp();
+
+			var exit = false;
+			while (!exit)
+			{
+				var userInput = MainMenu();
+				exit = TakeActions(db, userInput);
+			}
+		}
+
+		internal static CandyStorage SetupNewApp()
+		{
+			Console.Title = "Cross Confectioneries Incorporated";
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			var db = new CandyStorage();
+
+			return db;
+		}
+
+		internal static ConsoleKeyInfo MainMenu()
+		{
+			View mainMenu = new View()
+					.AddMenuOption("Did you just get some new candy? Add it here.")
+					.AddMenuOption("Do you want to eat some candy? Take it here.")
+					.AddMenuText("Press Esc to exit.");
+			Console.Write(mainMenu.GetFullMenu());
+			var userOption = Console.ReadKey();
+			return userOption;
+		}
+
+		private static bool TakeActions(CandyStorage db, ConsoleKeyInfo userInput)
+		{
+			Console.Write(Environment.NewLine);
+
+			if (userInput.Key == ConsoleKey.Escape)
+				return true;
+
+			var selection = userInput.KeyChar.ToString();
+			switch (selection)
+			{
+				case "1": AddNewCandy(db);
+					break;
+				case "2": EatCandy(db);
+					break;
+				default: return true;
+			}
+			return true;
+		}
+
+		internal static void AddNewCandy(CandyStorage db)
+		{
+			var newCandy = new Candy
+			{
+				Name = "Whatchamacallit"
+			};
+
+			var savedCandy = db.SaveNewCandy(newCandy);
+			Console.WriteLine($"Now you own the candy {savedCandy.Name}");
+		}
+
+		private static void EatCandy(CandyStorage db)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
