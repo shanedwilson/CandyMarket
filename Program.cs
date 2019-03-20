512d547b68706c418142034a2c3199d7d9896752
RichFisher <rich.fishernc@gmail.com>
2019-03-19 20:06:39 -0500

    lookating at shanes stuff

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using candy_market.candyStorage;
+using candy_market;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace candy_market
 {
@@ -7,13 +10,15 @@ namespace candy_market
 	{
 		static void Main(string[] args)
 		{
-			var db = SetupNewApp();
+            var candyOwners = new List<CandyStorage>();
+            var db = SetupNewApp();
+            candyOwners.Add(db);
 
-			var exit = false;
+            var exit = false;
 			while (!exit)
 			{
 				var userInput = MainMenu();
-				exit = TakeActions(db, userInput);
+				exit = TakeActions(db, userInput, candyOwners);
 			}
 		}
 
@@ -24,7 +29,6 @@ namespace candy_market
 			Console.ForegroundColor = ConsoleColor.Black;
 
 			var db = new CandyStorage();
-
 			return db;
 		}
 
@@ -39,7 +43,7 @@ namespace candy_market
 			return userOption;
 		}
 
-		private static bool TakeActions(CandyStorage db, ConsoleKeyInfo userInput)
+		private static bool TakeActions(CandyStorage db, ConsoleKeyInfo userInput, List<CandyStorage> candyOwners)
 		{
 			Console.Write(Environment.NewLine);
 
@@ -53,6 +57,8 @@ namespace candy_market
 					break;
 				case "2": EatCandy(db);
 					break;
+                case "3": TradeCandy(db, candyOwners);
+                    break;
 				default: return true;
 			}
 			return true;
@@ -60,18 +66,36 @@ namespace candy_market
 
 		internal static void AddNewCandy(CandyStorage db)
 		{
-			var newCandy = new Candy
-			{
-				Name = "Whatchamacallit"
-			};
-
-			var savedCandy = db.SaveNewCandy(newCandy);
-			Console.WriteLine($"Now you own the candy {savedCandy.Name}");
+            var newCandy = new Candy("Whatchamacallit", "Ass", "Hershey?");
+  
+			db.addCandy(newCandy);
+			Console.WriteLine($"Now you own the candy {newCandy.Name}");
 		}
 
 		private static void EatCandy(CandyStorage db)
 		{
 			throw new NotImplementedException();
 		}
+
+        internal static void TradeCandy(CandyStorage db, List<CandyStorage> candyOwners)
+        {
+            var menu = new View();
+            menu.AddMenuOption("Enter a candy owner's name to trade with.");
+            menu.AddMenuText("Press Esc to exit.");
+            Console.Write(menu.GetFullMenu());
+            var userOption = Console.ReadLine();
+            var otherOwner = candyOwners.Where(candies => candies.Owner == userOption).ToList();
+            writeCandies(otherOwner[0].Candies);
+        }
+
+        internal static void writeCandies(List<Candy> candies)
+        {
+            var counter = 0;
+            foreach(Candy candy in candies)
+            {
+                counter++;
+                Console.WriteLine($"{counter}. {candy.Name}, Flavor: {candy.Flavor}, Manufacturer {candy.Maker}, Received on {candy.Date}");
+            }
+        }
 	}
 }
