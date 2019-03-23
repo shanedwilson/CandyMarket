0def99d302973006586e3b974c030c28280d0e45
Marshall Offutt <marshalloffutt@gmail.com>
2019-03-23 13:50:58 -0500

    tweakz

--- Program.cs
+++ Program.cs
@@ -106,17 +106,24 @@ namespace candy_market
 
 		internal static void AddNewCandy(CandyStorage db, List<CandyStorage> candyOwners)
 		{
+            var addMenu = new View();
+            Console.Write(addMenu.GetFullMenu());
             Console.WriteLine("Might you know the name of the candy you wish to add?");
             var candyName = Console.ReadLine().ToString();
+            Console.Write(addMenu.GetFullMenu());
             Console.WriteLine("And might you know the manufacturer's name of the candy you wish to add?");
             var candyManufacturer = Console.ReadLine().ToString();
+            Console.Write(addMenu.GetFullMenu());
             Console.WriteLine("And might you know the flavor profile of the candy you wish to add?");
             var candyFlavor = Console.ReadLine().ToString();
 
             var newCandy = new Candy(candyName, candyFlavor, candyManufacturer );
 
 			db.addCandy(newCandy);
-			Console.WriteLine($"Now you own the candy {newCandy.Name}");
+            Console.Write(addMenu.GetFullMenu());
+            Console.WriteLine($"Now you own the candy {newCandy.Name}.");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue.");
             Console.ReadKey();
             var exit = false;
             while (!exit)
