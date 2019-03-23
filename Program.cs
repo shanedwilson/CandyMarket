9d44f94592a30c2a5a4f79f876c5f483da62278e
RichFisher <rich.fishernc@gmail.com>
2019-03-23 10:23:07 -0500

    merging stuff

--- Program.cs
+++ Program.cs
@@ -129,6 +129,12 @@ namespace candy_market
 
         internal static void TradeCandy(CandyStorage myStuff, List<CandyStorage> candyOwners)
         {
+            List<string> nameList = new List<string>();
+            foreach(CandyStorage  cstorage in candyOwners)
+            {
+                nameList.Add(cstorage.Owner);
+            }
+
             var menu = new View();
             menu.AddMenuText("Enter a candy owner's name to trade with.");
             menu.AddMenuText("Press Esc to exit.");
@@ -136,29 +142,37 @@ namespace candy_market
 
             var userOption = Console.ReadLine().ToLower();
             Console.WriteLine(userOption);
-            var otherOwner = candyOwners.Where(candies => candies.Owner == userOption).ToList()[0];
-            menu.AddMenuText("Select a candy number from the owner's list below.");
-            writeCandies(otherOwner.Candies, menu);
-            Console.Write(menu.GetFullMenu());
-            var otherOption = Int32.Parse(Console.ReadLine());
+            
+            if (nameList.Contains(userOption)) {
+                var otherOwner = candyOwners.Where(candies => candies.Owner == userOption).ToList()[0];
+                var otherNameArray = otherOwner.Owner.ToCharArray();
+                otherNameArray[0] = Char.ToUpper(otherNameArray[0]);
+                var otherName = string.Join("", otherNameArray);
+                menu.AddMenuText($"Select a candy number from {otherName}'s list below.");
+                writeCandies(otherOwner.Candies, menu);
+                Console.WriteLine(menu.GetFullMenu());
+                var otherOption = Int32.Parse(Console.ReadLine());
 
-            var menu2 = new View();
-            menu2.AddMenuText("Select a candy number from your list below.");
-            writeCandies(myStuff.Candies, menu2);
-            Console.Write(menu2.GetFullMenu());
-            var myOption = Int32.Parse(Console.ReadLine());
-            var otherNameArray = otherOwner.Owner.ToCharArray();
-            otherNameArray[0] = Char.ToUpper(otherNameArray[0]);
-            var otherName = string.Join("", otherNameArray);
+                var menu2 = new View();
+                menu2.AddMenuText("Select a candy number from your list below.");
+                writeCandies(myStuff.Candies, menu2);
+                Console.Write(menu2.GetFullMenu());
+                var myOption = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine($"You traded your {myStuff.Candies[myOption - 1].Name} for {otherName}'s {otherOwner.Candies[otherOption - 1].Name}.");
+                Console.WriteLine($"You traded your {myStuff.Candies[myOption - 1].Name} for {otherName}'s {otherOwner.Candies[otherOption - 1].Name}. Press enter to continue.");
 
-            otherOwner.addCandy(myStuff.Candies[myOption - 1]);
-            myStuff.addCandy(otherOwner.Candies[otherOption - 1]);
-            otherOwner.Candies.RemoveAt(otherOption - 1);
-            myStuff.Candies.RemoveAt(myOption - 1);
-            myStuff.orderCandy();
-            otherOwner.orderCandy();
+                otherOwner.addCandy(myStuff.Candies[myOption - 1]);
+                myStuff.addCandy(otherOwner.Candies[otherOption - 1]);
+                otherOwner.Candies.RemoveAt(otherOption - 1);
+                myStuff.Candies.RemoveAt(myOption - 1);
+                myStuff.orderCandy();
+                otherOwner.orderCandy();
+            } else
+            {
+                menu.AddMenuText("That name was not found. Press enter to continue");
+                Console.WriteLine(menu.GetFullMenu());
+            }
+            
 
             Console.ReadLine();
             var exit = false;
@@ -173,7 +187,7 @@ namespace candy_market
         {
             foreach(Candy candy in candies)
             {
-                menu.AddMenuOption($"{candy.Name}, Flavor: {candy.Flavor}, Manufacturer {candy.Maker}, Received on {candy.Date}");
+                menu.AddMenuOption($"{candy.Name}, Flavor: {candy.Flavor}, Manufacturer: {candy.Maker}, Received On: {candy.Date}");
             }
         }
 
@@ -272,4 +286,3 @@ namespace candy_market
         }
 	}       
 }
-
